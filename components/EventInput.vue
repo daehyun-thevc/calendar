e3ea6c6a6fb90dfd1e99d300edc72dc2bf5f1e08
daehyun-thevc <daehyun@thevc.kr>
2023-07-02 08:11:40 +0900

    feat: add event register

---
+++ components/EventInput.vue
@@ -0,0 +1,31 @@
+<script lang="ts" setup>
+const newEvent = ref("");
+const addNewEvent = () => {
+  const trimmed = newEvent.value.trim();
+  if (trimmed === "") return;
+  $fetch("/api/event", {
+    method: "POST",
+    body: {
+      title: trimmed,
+      description: "",
+    },
+  });
+};
+</script>
+
+<template>
+  <div class="flex gap-2 justify-center">
+    <div class="flex-1 max-w-[400px]">
+      <UInput
+        icon="i-jam-write"
+        size="md"
+        color="white"
+        :trailing="false"
+        :model-value="newEvent"
+        @update:model-value="newEvent = $event"
+        autofocus
+      />
+    </div>
+    <UButton @click="addNewEvent()">등록</UButton>
+  </div>
+</template>
