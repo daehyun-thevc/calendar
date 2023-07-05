883434dbaf587391b1c253d99b799bc5c50ba520
daehyun-thevc <daehyun@thevc.kr>
2023-07-05 14:52:45 +0900

    refactor: extract prisma to utils

--- server/api/event/index.get.ts
+++ server/api/event/index.get.ts
@@ -1,7 +1,3 @@
-import { PrismaClient } from "@prisma/client";
-
-const prisma = new PrismaClient();
-
 export default defineEventHandler((event) => {
   return prisma.event.findFirst({
     where: {
--- server/api/event/index.post.ts
+++ server/api/event/index.post.ts
@@ -1,7 +1,3 @@
-import { PrismaClient } from "@prisma/client";
-
-const prisma = new PrismaClient();
-
 export default defineEventHandler(async (event) => {
   const body = await readBody(event);
   const newEvent = await prisma.event.create({
